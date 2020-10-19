a61330bb2d71a78af36edc647d3ce4ddf5e2cea5
AlejandroOrtizBalmis <57657339+AlejandroOrtizBalmis@users.noreply.github.com>
2020-10-19 19:02:52 +0200

    Add files via upload

---
+++ Ejercicio4/Ejercicio4/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            caracteresTextBlock.Text =textoTextBox.Text.Length+"/140";
+            if (textoTextBox.Text.Length == 140) textoTextBox.IsReadOnly = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textoTextBox.IsReadOnly = false;
+            textoTextBox.Text = "";
+        }
+    }
+}
